6fb57567e3c396644b0a169d1badfe222c6328fb
Mateus Nava <nava.mateus@gmail.com>
2020-12-04 11:38:57 -0300

    chore: Remove primeicons

--- src/primereact-components.ts
+++ src/primereact-components.ts
@@ -1,6 +1,5 @@
 import 'primereact/resources/themes/saga-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
-import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
 
 export { Accordion } from 'primereact/accordion'
