2ed2bfe4e6b8f6925bfab3925750b0cd8ca9e326
GustavoChiochetta <gustavo.chiochetta751020@gmail.com>
2021-12-28 16:57:19 -0300

    [inovacao-T1604]Fix: ajustes de lint

--- src/primereact-components.ts
+++ src/primereact-components.ts
@@ -1,7 +1,7 @@
-import "primereact/resources/themes/lara-light-indigo/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
- 
+import 'primereact/resources/themes/lara-light-indigo/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+
 import configureLocale from './locale'
 configureLocale()
 
