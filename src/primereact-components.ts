755c21f9ab7df7962770859fc261dff1a368a665
GustavoChiochetta <gustavo.chiochetta751020@gmail.com>
2022-01-03 08:55:02 -0300

    [inovacao-T1604]Fix: downgrade primeflex e ajustes no export de componentes

--- src/primereact-components.ts
+++ src/primereact-components.ts
@@ -1,6 +1,5 @@
-import 'primereact/resources/themes/lara-light-indigo/theme.css'
 import 'primereact/resources/primereact.min.css'
-import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
 import configureLocale from './locale'
 configureLocale()
